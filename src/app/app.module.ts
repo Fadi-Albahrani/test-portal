568fc8cd213fdcdcd91c53498d95321e48322671
Fadi Albahrani <business.fadi21@gmail.com>
2021-08-11 15:52:54 +0300

    test & question interfaces added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ContentComponent } from './components/content/content.component';
+import { TestsListComponent } from './components/tests-list/tests-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    TestsListComponent
   ],
   imports: [
     BrowserModule,
